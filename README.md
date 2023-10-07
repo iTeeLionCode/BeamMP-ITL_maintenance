5588f74639e94b9093ce7224d6ee2670f02c0487
iTeeLion <dev@iteelion.ru>
2023-10-07 16:04:20 +0300

    initial

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# ITL_maintenance
+#ITL_maintenance
 
 Simple BeamMP serverside plugin for limited access by nicknames and custom maintenance message
---
+++ Resources/Server/ITL_maintenance/main.lua
@@ -0,0 +1,32 @@
+pluginName = "ITL_maintenance"
+pluginVersion = "0.0.1"
+
+CFG = require("cfg")
+
+function isUserAllowed(playerName)
+    for i = 1, #pluginConfig.allowedUsers do
+        if pluginConfig.allowedUsers[i] == playerName then
+            return true
+        end
+    end
+    return false
+end
+
+function onInit()
+
+    pluginConfig = CFG.getLocal("main")
+
+    print(pluginConfig)
+
+    function OnPlayerAuthHandler(playerName, playerRole, isGuest, identifiers)
+        if pluginConfig.isEnabled == true then
+            local res = isUserAllowed(playerName)
+            print(res)
+            if res ~= true then
+                return pluginConfig.maintenanceMessage
+            end
+        end
+    end
+    MP.RegisterEvent("onPlayerAuth", "OnPlayerAuthHandler")
+
+end
